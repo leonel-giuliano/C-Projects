a5252ad8b2855fff94c1d374b965aedb05519ae5
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-05-22 10:35:42 -0300

    Error handler

---
+++ handle-csv/error.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "error.h"
+
+void errorHandler(errorEvent_t error) {
+    puts("ERROR: ");
+
+    switch(error) {
+        case ERROR_ARGC:
+            puts("Usage: main.exe [file.csv].");
+            break;
+
+        case ERROR_FILE:
+            puts("There was a problem with the file.");
+            break;
+
+        default:
+            puts("Unkown error.");
+            break;
+    }
+
+    exit(EXIT_FAILURE);
+}
---
+++ handle-csv/error.h
@@ -0,0 +1,6 @@
+typedef enum {
+    ERROR_ARGC,
+    ERROR_FILE
+}errorEvent_t;
+
+void errorHandler(errorEvent_t _Error);
