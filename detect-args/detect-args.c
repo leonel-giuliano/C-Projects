89b65dc93a19c2bbd78eac468a8c3d895e6dee38
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-07-23 02:53:13 -0300

    Array to list the operations

--- detect-args/detect-args.c
+++ detect-args/detect-args.c
@@ -6,13 +6,6 @@
 #include "detect-args.h"
 
 
-// Strings
-#define STR_MAX 64
-
-
-// Inside functions
-
-
 // Functions
 
 uint8_t detectArgs(int argc, char *argv[], argOperation_t *argOp, uint8_t nOp, ...) {
@@ -20,7 +13,16 @@ uint8_t detectArgs(int argc, char *argv[], argOperation_t *argOp, uint8_t nOp, .
     va_list(op);
     va_start(op, nOp);
 
+    // Callocate the array of the list of operations
     char ***operations;
+    if((operations = (char ***)calloc(nOp, sizeof(char **))) == NULL)
+        return EXIT_FAILURE;
+
+    // Save the list of operations
+    for(uint8_t i = 0; i < nOp; i++)
+        operations[i] = va_arg(op, char **);
 
     va_end(op);
+
+    free(operations);
 }
