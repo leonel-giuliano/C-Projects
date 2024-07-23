731eba752e30f7a58493cbf06ca608589fdee5b2
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-07-23 16:33:07 -0300

    errorHandler and detect the arguments

--- ninstall/ninstall.c
+++ ninstall/ninstall.c
@@ -1,34 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+#include "ninstall.h"
 #include "detect-args.h"
 
 
 int main(int argc, char *argv[]) {
-    const char *comm[] = {
+    if(argc < ARGC_MIN || argc > ARGC_MAX) errorHandler(ERROR_ARG);
+
+    // Set all the list of operations
+    const char *command = {
         "new",
         "edit",
+        "remove",
+        "show",
         NULL
     };
 
-    const char *option1[] = {
+    const char *option1 = {
         "-h",
         "-l",
         NULL
     };
 
-    const char *option2[] = {
+    const char *option2 = {
         "--help",
         "--list",
         NULL
     };
 
-    argOperation_t argOp[3];
-    detectArgs(argc, argv, argOp, 3, comm, option1, option2);
-
-    for(int i = 1; i < argc; i++) {
-        printf("Type: %d\n", argOp[i - 1].type);
-        printf("Operation: %d\n\n", argOp[i - 1].operation);
-    }
+    // Check the arguments operations
+    argOperation_t argOp[AMOUNT_OPERATION];
+    uint8_t detectError = detectArgs(argc, argv, argOp, AMOUNT_OPERATION, command, option1, option2);
+    if(detectError) errorHandler(ERROR_MEMORY);
 
     return 0;
+}
+
+void errorHandler(error_t error) {
+    printf("ERROR: ");
+
+    switch(error) {
+        case ERROR_ARG:
+            puts("There was a bad usage when using ninstall.\n");
+            break;
+
+        case ERROR_MEMORY:
+            perror("There was a problem when allocating memory.");
+            break;
+
+        default:
+            perror("[DEBUG] strange outcome from the errorHandler.");
+            break;
+    }
+
+    exit(EXIT_FAILURE);
 }
---
+++ ninstall/ninstall.h
@@ -0,0 +1,38 @@
+#ifndef __NINSTALL_H__
+#define __NINSTALL_H__
+
+
+// Arguments
+
+// ninstall --help
+#define ARGC_MIN 2
+// ninstall new program
+#define ARGC_MAX 3
+
+
+// Operations
+
+#define AMOUNT_OPERATION 3
+
+typedef enum {
+    IX_NONE,
+    IX_COMMAND,
+    IX_OPTION1,
+    IX_OPTION2
+};
+
+
+// Error events
+typedef enum {
+    ERROR_ARG,
+
+    ERROR_MEMORY
+}error_t;
+
+
+// Functions
+
+void errorHandler(error_t _ErrorEvent);
+
+
+#endif
