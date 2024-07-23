ffcd8f1d8ca5fd7be9181252183cfa2630421857
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-07-23 03:48:02 -0300

    Cmake for detect-args

--- ninstall/ninstall.c
+++ ninstall/ninstall.c
@@ -3,26 +3,32 @@
 #include "detect-args.h"
 
 
-int main(void) {
-    const char *comm = {
+int main(int argc, char *argv[]) {
+    const char *comm[] = {
         "new",
         "edit",
         NULL
     };
 
-    const char *option1 = {
+    const char *option1[] = {
         "-h",
         "-l",
         NULL
     };
 
-    const char *option2 = {
+    const char *option2[] = {
         "--help",
         "--list",
         NULL
     };
 
-    
+    argOperation_t argOp[3];
+    detectArgs(argc, argv, argOp, 3, comm, option1, option2);
+
+    for(int i = 1; i < argc; i++) {
+        printf("Type: %d\n", argOp[i - 1].type);
+        printf("Operation: %d\n\n", argOp[i - 1].operation);
+    }
 
     return 0;
 }
