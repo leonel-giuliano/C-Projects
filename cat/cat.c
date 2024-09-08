09211c6a2d5c25398a09dee73c8b230f2ad0f7d6
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-09-07 23:38:09 -0300

    Added the libargop and created function to know the arguments operations

--- cat/cat.c
+++ cat/cat.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "argop.h"
+
 #include "cat.h"
 
 
 int main(int argc, char *argv[]) {
     if(argc < ARGC_MIN || argc > ARGC_MAX) errorHandler(ERROR_ARG);
 
+    // Check the operation of every argument
+    // The "- 1" is to ignore the command
+    argOp_t argOp[ARGC_MAX - 1];
+    initFlags(argc, argv, argOp);
+
     return 0;
+}
+
+
+void initFlags(int argc, char *argv[], argOp_t argOp[]) {
+    const char *flags[] = {
+        "--help",
+        "--version",
+        NULL
+    };
+
+    const char *option1[] = {
+        "--show-all",
+        "--number-nonblank",
+        "--show-ends",
+        "--number",
+        "--squeeze-blank",
+        "--show-tabs",
+        "--show-nonprinting",
+        NULL
+    };
+
+    // The "- 1" is because of the mult options
+    checkArgs(argc, argv, argOp, OP_AMOUNT - 1, flags, option1);
 }
 
 
--- cat/cat.h
+++ cat/cat.h
@@ -13,12 +13,21 @@
 #define ARGC_MAX 3
 
 
+// Operations
+
+#define OP_AMOUNT 3
+
+
 // Error events
 typedef uint8_t error_t;
 enum {
     ERROR_ARG
 };
 
+
+// Contains the operations array and calls for 'checkArgs()'
+// This function doesn't check for the mult options
+void initFlags(int argc, char *argv[], argOp_t _ArgOperations[]);
 
 void errorHandler(error_t _ErrorEvent);
 
