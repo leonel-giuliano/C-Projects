4d37530035a324c6d15f7ec57e767d901dc26f25
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-09-26 21:01:23 -0300

    Error handler

--- pura-garra/main.c
+++ pura-garra/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
 
+#include "main.h"
 #include "sales.h"
 
 
 int main() {
     return 0;
+}
+
+void errorHandler(errorEvent_t error, ...) {
+    va_list arg;
+    va_start(arg, error);
+
+    va_end(arg);
+
+    return 1;
 }
